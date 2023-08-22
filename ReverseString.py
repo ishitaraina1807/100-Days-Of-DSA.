893ee49137dcb0e045937d12532b6eecd5f280e4
Mermaido <ishitaraina18@gmail.com>
2023-08-22 20:23:52 +0530

    Update ReverseString.py

--- ReverseString.py
+++ ReverseString.py
@@ -1,3 +1,10 @@
-s = ['i','s','h','i','t','a']
-q = s[::-1]
-print(q)
+def reverseList(lst, i, j):
+    while i < j:
+        lst[i], lst[j] = lst[j], lst[i]
+        i += 1
+        j -= 1
+lst = ['i', 's', 'h', 'i', 't', 'h' ,'a']
+print(lst)
+reverseList(lst, 0, 6)
+print("Reversed list is")
+print(lst)
