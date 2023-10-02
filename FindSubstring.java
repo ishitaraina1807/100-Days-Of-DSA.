a7a9ee509a7771a57fcbbe2d38ac5ba59500123b
Mermaido <ishitaraina18@gmail.com>
2023-10-02 16:33:39 +0530

    Create FindSubstring.java

---
+++ FindSubstring.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int strStr(String haystack, String needle) {
+       int index = haystack.indexOf(needle);
+       if (index == -1) {
+           return -1;
+       }
+       return index;
+        }
+    }
